69f44e7d79a506b8408fb8e65689a0dc976d6822
jerom <jlardenoije@gmail.com>
2025-01-16 23:39:35 +0100

    navigation test

--- script.js
+++ script.js
@@ -4,12 +4,14 @@ console.log("test");
 
 var button = document.getElementById("testClass");
 var div = document.getElementById("json");
+var nav = document.getElementById("navigation");
 button.innerHTML = "haha yes";
 
 button.onclick = async function() 
 { 
   var response = await fetch("recipes.json");
   var json = await response.json();
+  json.Raiko.foreach((e) => nav.innerHTML += "<a href='Raiko/SpaghettiCarbonara.html'>link</a>");
   console.log(json);
   alert('clicked'); 
 };
