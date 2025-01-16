5bafe5e01e3222a585015885c647cf8743b6d9c4
jerom <jlardenoije@gmail.com>
2025-01-16 22:59:44 +0100

    surely

--- script.js
+++ script.js
@@ -2,17 +2,12 @@ var recipes = null;
 
 console.log("test");
 
-document.addEventListener('DOMContentLoaded', function() {
-     fetch('recipes.json') // Fetch the JSON file
-         .then(response => { console.log(response); response.json() }) // Convert response to JSON
-         .then(data => {
-           console.log(data);
-           recipes = data;
-      })
-      .catch(error => console.error('Error fetching recipes:', error)); // Handle any errors
-});
-
 var button = document.getElementById("testClass");
 button.innerHTML = "haha yes";
 
-button.onclick = function() { alert('clicked'); };
+button.onclick = function() 
+{ 
+  fetch("recipes.json")
+    .then((value) => { console.log(value)});
+  alert('clicked'); 
+};
