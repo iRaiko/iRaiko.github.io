09772e9cbc3f07d62c0d47b497484641e152ee6c
jerom <jlardenoije@gmail.com>
2025-01-16 22:42:26 +0100

    Further testing

--- script.js
+++ script.js
@@ -1,9 +1,16 @@
+var recipes = null;
+
+console.log("test");
+
 document.addEventListener('DOMContentLoaded', function() {
      fetch('recipes.json') // Fetch the JSON file
          .then(response => response.json()) // Convert response to JSON
          .then(data => {
            console.log(data);
+           recipes = data;
       })
       .catch(error => console.error('Error fetching recipes:', error)); // Handle any errors
-
 });
+
+var button = document.getElementById("testClass");
+button.innerHTML = "haha yes";
