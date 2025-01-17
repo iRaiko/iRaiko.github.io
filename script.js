d873d03c1a96cffa65ff5efe6fc0ef4ff044a22a
jerom <jlardenoije@gmail.com>
2025-01-17 12:01:26 +0100

    catagories object

--- script.js
+++ script.js
@@ -12,12 +12,18 @@ button.onclick = async function()
   var json = await response.json();
   for(const person in json) {
     var li_person_name = document.createElement("li");
+    var ul_person_name = document.createElement("ul");
+    ul_person_name.classList.add("nestlist");
     var a_person_name = document.createElement("a");
     a_person_name.href = "#";
     a_person_name.textContent = person;
     li_person_name.appendChild(a_person_name);
+    li_person_name.appendChild(ul_person_name);
     for(const recipe of json[person]) {
       nav.innerHTML += "<a href=" + person + "/" + recipe[0] + ">" + recipe[0] + "</a>";
+      for(const t of recipe.slice(1)) {
+        catagories[person][t] = recipe[0];
+      }
     }
     var navbar = document.getElementById("navbar");
     console.log(navbar);
@@ -37,7 +43,7 @@ var template = document.getElementById("buttonTemplate");
 var buttons = document.getElementById("buttons");
 buttons.appendChild(template.cloneNode(true));
 
-
+console.log(catagories);
 
 
 
