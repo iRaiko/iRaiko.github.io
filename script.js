413feaedbbde6ad02b1e11d1bf113dbc3485f3f5
jerom <jlardenoije@gmail.com>
2025-01-17 11:13:00 +0100

    Add navigation

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ console.log("test");
 var button = document.getElementById("getJson");
 var div = document.getElementById("json");
 var nav = document.getElementById("navigation");
-var nav = document.getElementById("navbar");
+var navbar = document.getElementById("navbar");
 button.innerHTML = "get json";
 
 button.onclick = async function() 
@@ -14,9 +14,16 @@ button.onclick = async function()
   var response = await fetch("recipes.json");
   var json = await response.json();
   for(const person in json) {
+    const li_person_name = document.createElement("li");
+    const a_person_name = document.createElement("a");
+    a_person_name.href = "#";
+    a_person_name.textContent = person;
+    li_person_name.appendChild(a_person_name);
     for(const recipe of json[person]) {
       nav.innerHTML += "<a href=" + person + "/" + recipe[0] + ">" + recipe[0] + "</a>";
     }
+
+    navbar.appendChild(li_person_name);
   }
   console.log(json);
   alert('clicked'); 
